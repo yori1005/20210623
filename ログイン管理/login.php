326fd78925c3497433ee65c7f977ec893c23a8f1
yori1005 <86332919+yori1005@users.noreply.github.com>
2021-07-20 00:32:48 +0900

    Add files via upload

--- ログイン管理/login.php
+++ ログイン管理/login.php
@@ -1,13 +1,38 @@
 <?php
 session_start();
-require "DataBase.php";
-$db = new DataBase();
+
+$dsn = 'mysql:host=157.112.147.201;
+        dbname=g079ff_2020';
+$user =  'g079ff_ymgc';
+$pass = 'kpEYZ8KU';
+try {
+    $dbh = new PDO($dsn, $user, $pass);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+
 if ($_POST['id'] != NULL && $_POST['pass'] != NULL) {
-    if ($db->dbConnect()) {
-        if ($db->logIn("user", $_POST['id'], $_POST['pass'])) {
-           	header('Location: http://g079ff.php.xdomain.jp/mypage.php');
-			exit;
-        } else{header('Location: http://g079ff.php.xdomain.jp/login_page.php');}
-    } else echo "Error: Database connection";
-} else echo "All fields are required";
+
+        $id = $_POST['id'];
+		$pass = stripslashes(htmlspecialchars($_POST['pass']));
+        $sql = "SELECT * FROM user WHERE id = '$id'";
+		$stmt = $dbh->query($sql);
+		foreach($stmt as $row){
+			$dbid = $row['id'];
+			$dbpassword = $row['pass'];
+			$dbname = $row['name'];
+		}
+            if ($dbid == $id && password_verify($pass, $dbpassword)) {
+				$_SESSION['id'] = $dbid;
+				$_SESSION['pass'] = $dbpassword;
+				$_SESSION['name'] = $dbname;
+				header('Location: http://g079ff.php.xdomain.jp/mypage.php');
+            }else{
+				header('Location: http://g079ff.php.xdomain.jp/login_page.php');
+			} 
+        }else{
+			header('Location: http://g079ff.php.xdomain.jp/login_page.php');
+		}
+
 ?>
--- ログイン管理/signup.php
+++ ログイン管理/signup.php
@@ -1,12 +1,40 @@
 <?php
-require "DataBase.php";
-$db = new DataBase();
-if ($_POST['name'] != NULL && $_POST['id'] != NULL && $_POST['pass'] != NULL) {
-    if ($db->dbConnect()) {
-        if ($db->signUp("user", $_POST['name'], $_POST['id'], $_POST['pass'])) {
-			header('Location: http://g079ff.php.xdomain.jp/login_page.php');
-        } else{	header('Location: http://g079ff.php.xdomain.jp/Home.php');}
-    } else echo "Error: Database connection";
-} else{ 
+session_start();
+
+$dsn = 'mysql:host=157.112.147.201;
+        dbname=g079ff_2020';
+$user =  'g079ff_ymgc';
+$pass = 'kpEYZ8KU';
+try {
+    $dbh = new PDO($dsn, $user, $pass);
+} catch (PDOException $e) {
+    echo $e->getMessage();
 }
+
+if ($_POST['name'] != NULL && $_POST['id'] != NULL && $_POST['pass'] != NULL) {
+	$name = $_POST['name'];
+	$id = $_POST['id'];
+	$pass = stripslashes(htmlspecialchars($_POST['pass']));
+	$pass = password_hash($pass, PASSWORD_DEFAULT);
+	$sql = "SELECT * FROM user WHERE id = '$id'";
+	$stmt = $dbh->query($sql);
+	foreach($stmt as $row){
+		$dbid = $row['id'];
+		$dbpassword = $row['pass'];
+		$dbname = $row['name'];
+	}
+	if($dbid == $id){
+		header('Location: http://g079ff.php.xdomain.jp/Home.php');
+	}
+	else{
+		$sql = "INSERT INTO user(name,id,pass) VALUES (:name,:id,:pass)";
+		$stmt = $dbh->prepare($sql);
+		$params = array(':name' => $name,':id' => $id,':pass' => $pass);
+		$stmt->execute($params);
+		header('Location: http://g079ff.php.xdomain.jp/login_page.php');
+	}
+}else{
+	header('Location: http://g079ff.php.xdomain.jp/signup_page.php');
+}
+
 ?>
